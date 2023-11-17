5300762f461190508fd2cd684662a222be2a646c
Andrijana Gjeorgieva <46373455+sweetdandelions@users.noreply.github.com>
2023-11-17 12:08:28 +0100

    Update SendLongRunningReportLastHour.sql

--- SendLongRunningReportLastHour.sql
+++ SendLongRunningReportLastHour.sql
@@ -2,7 +2,7 @@
 CREATE PROCEDURE dbo.SendLongRunningReportLastHour
 AS
 BEGIN
-    -- Threshold for duration of a job
+    -- Threshold for duration of a job in minutes
     DECLARE @ThresholdDuration INT = 30;
 
     -- Create a table variable to store long running jobs
@@ -12,14 +12,14 @@ BEGIN
         JobName NVARCHAR(128),
         StartTime NVARCHAR(128),
         RunStatus NVARCHAR(MAX),
-		Duration NVARCHAR(MAX),
-		RunDate DATE
+	Duration NVARCHAR(MAX),
+	RunDate DATE
     );
 
     -- Fetch long running jobs from all job IDs within the last hour
     INSERT INTO @LongRunningMessages (JobName, StartTime, RunStatus, Duration, RunDate)
     
-select 
+SELECT 
 	j.name,
 	FORMAT(a.start_execution_date, 'HH:mm:ss tt') AS StartTime,
 	CASE
@@ -27,59 +27,53 @@ select
 		WHEN a.start_execution_date IS NOT NULL AND a.stop_execution_date IS NULL THEN 'Running'
 		WHEN a.start_execution_date IS NOT NULL AND a.stop_execution_date IS NOT NULL THEN 'Not running'
 	END AS 'RunStatus',
-	CAST(DATEPART(HOUR, a.start_execution_date) AS VARCHAR(2)) + 'h ' +
-    	CAST(DATEPART(MINUTE, a.start_execution_date) AS VARCHAR(2)) + 'min ' +
-    	CAST(DATEPART(SECOND, a.start_execution_date) AS VARCHAR(2)) + 'sec' AS Duration,
-	--FORMAT(DATEADD(SECOND, DATEDIFF(SECOND, a.start_execution_date, GETDATE()), '19000101'), 'HH:mm:ss') AS Duration,
+	FORMAT(DATEADD(SECOND, DATEDIFF(SECOND, a.start_execution_date, GETDATE()), '19000101'), 'HH:mm:ss') AS Duration,
 	FORMAT(a.start_execution_date, 'yyyy-MM-dd') AS RunDate
-from msdb.dbo.sysjobs j
-join msdb.dbo.sysjobactivity a
-on j.job_id = a.job_id
+FROM msdb.dbo.sysjobs j
+JOIN msdb.dbo.sysjobactivity a
+ON j.job_id = a.job_id
 WHERE 
-	session_id = (
-	SELECT MAX(session_id) FROM msdb.dbo.sysjobactivity)
-	AND 
-	(a.start_execution_date IS NOT NULL AND a.stop_execution_date IS NULL)
+	session_id = (SELECT MAX(session_id) FROM msdb.dbo.sysjobactivity)
+	AND (a.start_execution_date IS NOT NULL AND a.stop_execution_date IS NULL)
 
 
     -- If there are long running jobs, send a single email notification with a formatted HTML table
     IF EXISTS (SELECT 1 FROM @LongRunningMessages)
     BEGIN
-		DECLARE @subject VARCHAR(max);
-		SET @subject = 'Long running jobs - ' + CONVERT(VARCHAR(12),GETDATE(),107);
+	DECLARE @subject VARCHAR(max);
+	SET @subject = 'Long running jobs - ' + CONVERT(VARCHAR(12),GETDATE(),107);
 
-        DECLARE @message NVARCHAR(MAX);
-		
+        DECLARE @message NVARCHAR(MAX);	
         SET @message = '<html><body>' +
             '<p>Long running jobs for the last hour: </p>' +
             '<table style="border-collapse: collapse; padding : 0px">' +
-            '<tr>
-			<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Job Name</th>
-			<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Status</th>
-			<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Duration</th>
-			<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Start Time</th>
-			<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Date</th>
-			</tr>';
+        '<tr>
+		<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Job Name</th>
+		<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Status</th>
+		<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Duration</th>
+		<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Start Time</th>
+		<th style="border: 2px solid #8c0416;text-align:center;padding: 5px;background-color: #D70040;color: #FFFFFF;">Date</th>
+	</tr>';
 
         -- Construct the message with long running jobs, timestamps, job names, and duration in an HTML table
         SELECT @message = @message + 
-            '<tr>
-				<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + JobName + '</td>' +
-				'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + RunStatus + '</td>' +
-				'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + Duration + '</td>' +
-				'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + StartTime + '</td>' +
-				'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + CONVERT(NVARCHAR, RunDate, 120) + '</td>
-			</tr>'
+        '<tr>
+		<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + JobName + '</td>' +
+		'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + RunStatus + '</td>' +
+		'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + Duration + '</td>' +
+		'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + StartTime + '</td>' +
+		'<td style="border: 2px solid #8c0416;text-align:center;padding: 5px;">' + CONVERT(NVARCHAR, RunDate, 120) + '</td>
+	</tr>'
         FROM @LongRunningMessages;
 
         SET @message = @message + '</table></body></html>';
 
         -- Send a single email notification with the formatted HTML table
          EXEC msdb.dbo.sp_send_dbmail
-			@profile_name = 'SQL Alerts',
-			@recipients = 'test@gmail.com',
-			@subject = @subject,
-			@body = @message,
-            @body_format = 'HTML';
+		@profile_name = 'SQL Alerts',
+		@recipients = 'test@gmail.com',
+		@subject = @subject,
+		@body = @message,
+            	@body_format = 'HTML';
     END;
 END;
