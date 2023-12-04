d28ee584bce0f896d2d24d9537dfb40229e92c40
Andrijana Gjeorgieva <46373455+sweetdandelions@users.noreply.github.com>
2023-12-04 15:07:04 +0100

    Update SnowflakeLongRunningUserQuery.sql

--- SnowflakeLongRunningUserQuery.sql
+++ SnowflakeLongRunningUserQuery.sql
@@ -1,4 +1,21 @@
--- Retrieve user, warehouse, query, start time, and duration for queries running longer than 1 hour
+-- Current long running queries
+SELECT
+  USER_NAME AS "User",
+  DATABASE_NAME AS "Database",
+  WAREHOUSE_NAME AS "Warehouse",
+  QUERY_TYPE AS "Operation",
+  QUERY_TEXT AS "Query",
+  TO_CHAR(START_TIME, 'HH:MI:SS AM') AS "Start Time",
+  EXECUTION_STATUS AS "Status",
+  DATEDIFF('seconds', START_TIME, CURRENT_TIMESTAMP()) AS "Duration"
+FROM TABLE(snowflake.INFORMATION_SCHEMA.QUERY_HISTORY())
+WHERE EXECUTION_STATUS IN ('RUNNING', 'QUEUED', 'BLOCKED')
+AND USER_NAME NOT IN ('SNAPLOGIC')
+AND DATEDIFF('seconds', START_TIME, CURRENT_TIMESTAMP()) > 120
+ORDER BY START_TIME DESC;
+
+
+-- Retrieve user, warehouse, query, start time, and duration for queries running longer than 1 hour: HISTORY
 SELECT
   USER_NAME AS "User",
   WAREHOUSE_NAME AS "Warehouse",
@@ -10,7 +27,7 @@ FROM TABLE(INFORMATION_SCHEMA.QUERY_HISTORY())
 WHERE DATEDIFF('hour', START_TIME, END_TIME) > 1  -- Filter for queries longer than 1 hour
 ORDER BY START_TIME DESC;
 
--- Retrieve user, warehouse, query, start time, and formatted duration for queries running longer than 1 hour
+-- Retrieve user, warehouse, query, start time, and formatted duration for queries running longer than 1 hour: HISTORY
 SELECT
   USER_NAME AS "User",
   WAREHOUSE_NAME AS "Warehouse",
@@ -25,6 +42,7 @@ FROM TABLE(INFORMATION_SCHEMA.QUERY_HISTORY())
 WHERE DATEDIFF('hour', START_TIME, END_TIME) > 1  -- Filter for queries longer than 1 hour
 ORDER BY START_TIME DESC;
 
+-- HISTORY AGAIN
 SELECT
   USER_NAME AS "User",
   DATABASE_NAME AS "Database",
