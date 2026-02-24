2df329ab26c85e3b4e0b066c8ecac319fd7ed00c
Andrijana Gjeorgieva <46373455+sweetdandelions@users.noreply.github.com>
2026-02-24 13:43:19 +0100

    Update GetColumns.sql

--- GetColumns.sql
+++ GetColumns.sql
@@ -1,4 +1,6 @@
-SELECT LISTAGG(column_name, ', '), table_name
+SELECT table_name, LISTAGG(column_name, ', ')
+       WITHIN GROUP (ORDER BY ordinal_position)
 FROM information_schema.columns
-WHERE table_name in('tableA', 'tableB') 
+WHERE table_schema = 'schema'
+AND table_name in('tableA', 'tableB') 
 AND column_name NOT IN ('load_dt') group by table_name;
